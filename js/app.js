58fa4f8b6c0cddb292748058ff318e18e476b056
gudintak <52444596+gudintak@users.noreply.github.com>
2020-05-11 01:59:40 +0530

    Mods in app.js and index.html

--- js/app.js
+++ js/app.js
@@ -1,67 +1,24 @@
-//List items to DOM
-const navbar__list = document.getElementById('navbar__list');
-let newListItem = document.createElement('li');
-newListItem.innerHTML = `<a href="#section1">Section 1</a>
-<a href="#section2">Section 2</a>
-<a href="#section3">Section 3</a>
-<a href="#section4">Section 4</a>`;
-navbar__list.appendChild(newListItem);
+//List generated Dynamically
 
-//Navigation on smaller screens
-const navbarSwitch = document.querySelector('.navbar-toggler');
-const navbarMenu = document.querySelector('#navbar__list');
-const navbarLinks = document.querySelectorAll('.navbar__menu a');
+const navbar__list = document.getElementsByClassName('navbar-nav')[0];
+var sections = document.querySelectorAll('.landing__container h2');
+var ph = '';
+for (section of sections) {
+  var sectionstr = section.innerHTML
+  var splits = sectionstr.split(" ");
+  var newName = splits[0].toLowerCase() + splits[1]
+  // console.log(newName);
+  ph = ph + '<li class="nav-item"><a class="nav-link" href="#' + newName + '">' + sectionstr + '</a></li>'
+};
+console.log(ph);
+navbar__list.innerHTML = ph;
 
-navbarSwitch.addEventListener("click", navbarSwitchClick);
+$(document).ready(function(){
 
-function navbarSwitchClick() {
-    navbarSwitch.classList.toggle("open-navbar-toggler");
-    navbarMenu.classList.toggle("open");
-}
+   $("div").mouseenter(function(){
+   	 var id = $(this).attr('id');
+   	 $('a').removeClass('active');
+   	 $("[href=#"+id+"]").addClass('active');
+   });
 
-navbarLinks.forEach(elem => elem.addEventListener("click", navbarLinkClick));
-
-function navbarLinkClick() {
-    smoothScroll(event);
-    if(navbarMenu.classList.contains("open")){
-        navbarSwitch.click();
-    }
-}
-
-smoothScroll = function(event) {
-    event.preventDefault();
-    const targetId = event.currentTarget.getAttribute("href")==="#" ? "body" : event.currentTarget.getAttribute("href");
-    document.querySelector(targetId).scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-    });
-}
-
-
-
-/// script for active navigation style scroll
-
-window.addEventListener('scroll', function() {
-    let nav = document.querySelector('.navbar__menu');
-
-    (window.scrollY >= 1) ? nav.classList.add('scroll') : nav.classList.remove('scroll');
-});
-
-
-window.addEventListener('scroll', function(event) {
-    let navLinks = document.querySelectorAll('nav ul li a');
-    let fromTop = window.scrollY;
-
-    navLinks.forEach(link => {
-        let section = document.querySelector(link.hash);
-
-        if (
-            section.offsetTop <= fromTop &&
-            section.offsetTop + section.offsetHeight > fromTop
-        ) {
-            link.classList.add('current');
-        } else {
-            link.classList.remove('current');
-        }
-    });
 });
